15064362571ae43ecc559eba763f4f5f64e8323f
Julio Peraza <julioperaza@gmail.com>
2020-10-16 19:08:33 -0600

    viernes

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const app = express();
 const fs = require('fs');
@@ -50,69 +49,19 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
-process.on('SIGINT', function () {
-  socket.close();
-  process.exit();
-});
-
-const port = 4040;
-app.listen(port, () => {
-  console.log(`Running on port ${port}`);
-});
-=======
-const express = require('express');
-const app = express();
-const fs = require('fs');
-
 app.use(express.json());
-// app.get('/', (req, res) => {
-//   res.status(404).json({
-//     mensaje: 'Esto es el servidor subido con node y express',
-//     App: 'Natours',
-//   });
-// });
+app.get('/', (req, res) => {
+  res.status(404).json({
+    mensaje: 'Esto es el servidor subido con node y express',
+    App: 'Natours',
+  });
+});
 
 // app.post('/', (req, res) => {
 //   res.send('Esta es la prueba');
 // });
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json({
-    Results: tours.length,
-    Status: 'Exito',
-    data: {
-      tours,
-    },
-  });
-});
-
-app.post('/api/v1/tours', (req, res) => {
-  var tourId = tours[tours.length - 1].id + 1;
-
-  const newTour = Object.assign({ id: tourId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(newTour),
-    (err) => {
-      res.status(201).json({
-        status: 'sucess',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-});
 
 const port = 3030;
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
->>>>>>> 329c8a27b0fe4cf542d0fe4350891064bd97fab2

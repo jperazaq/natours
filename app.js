64bfba520a8b06483fe5ed38a1cfed506a7fa8b3
Julio Peraza <julioperaza@gmail.com>
2020-11-06 21:56:54 -0600

    11/06/2020

--- app.js
+++ app.js
@@ -1,67 +1,67 @@
+const { deepStrictEqual } = require('assert');
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const morgan = require('morgan');
+app.use(express.json());
+
 
 app.use(express.json());
-// app.get('/', (req, res) => {
-//   res.status(404).json({
-//     mensaje: 'Esto es el servidor subido con node y express',
-//     App: 'Natours',
-//   });
-// });
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-// app.post('/', (req, res) => {
-//   res.send('Esta es la prueba');
-// });
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json({
-    Results: tours.length,
-    Status: 'Exito',
+app.get('/api/v1/tours',(req,res)=>{
+res.status(200).json({
+    results : tours.length,
+    status: "todos tush",
     data: {
-      tours,
-    },
-  });
-});
-
-app.post('/api/v1/tours', (req, res) => {
-  var tourId = tours[tours.length - 1].id + 1;
-
-  const newTour = Object.assign({ id: tourId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(newTour),
-    (err) => {
-      res.status(201).json({
-        status: 'sucess',
-        data: {
-          tour: newTour,
-        },
-      });
+        tours
     }
-  );
+});
 });
 
-app.use(express.json());
-app.get('/', (req, res) => {
-  res.status(404).json({
-    mensaje: 'Esto es el servidor subido con node y express',
-    App: 'Natours',
-  });
-});
 
-// app.post('/', (req, res) => {
-//   res.send('Esta es la prueba');
-// });
+app.get('/api/v1/tours/:id',(req,res)=>{
+
+    console.log(req.params);
+
+    const id = req.params.id *1;
+
+    if(id>tours.length){
+        return res.status(404).json({
+            status :"fallo",
+            message: "No existe lo que buscas"
+        })
+    }
+    const tour = tours.find(el => el.id === id);
+
+    res.status(200).json({
+        // results : tours.length,
+        status: "todos tush",
+         data: {
+            tour
+         }
+    });
+    });
+
+app.post('/api/v1/tours', (req,res)=>{
+    // console.log(req.body);
+
+    const newID = tours[tours.length-1].id + 1;
+    const newTour = Object.assign({id : newID}, req.body);
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), error=>{
+        res.status(201).json({
+            status:"Grande muy bien en los detalles",
+            data:{
+                tour: newTour
+            }
+        })
+    })
+    
+})
 
 const port = 3030;
 app.listen(port, () => {
-  console.log(`Running on port ${port}`);
+    console.log(`Running on port ${port}`);
 });
